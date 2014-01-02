0b65b3d00ff50508d7e5a55698e6af51b0abd4bd
hunterboerner <hunterboerner@gmail.com>
2014-01-02 12:46:25 -0600

    Derp name

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ApplicationHelperSpec do
+describe ApplicationHelper do
   describe "Page" do
     before { `git checkout c413d31` }
     @branch = `git rev-parse --abbrev-ref HEAD`
