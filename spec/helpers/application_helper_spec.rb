fd03fc375a60ab4a94b811ca1425eccdb403ced5
hunterboerner <hunterboerner@gmail.com>
2014-01-02 13:26:42 -0600

    Be a good coveralls now...

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe "Git SHA" do
+  describe "git SHA" do
     it 'should be the correct length' do
       expect(get_SHA.length).to eq(40)
       expect(get_short_SHA.length).to eq(7)
