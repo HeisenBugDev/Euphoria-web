31829f714f3fd3fe60e0806ea777f81955e46ec8
hunterboerner <hunterboerner@gmail.com>
2014-01-02 13:10:09 -0600

    Stop being a derp coveralls

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe "git SHA" do
+  describe "Git SHA" do
     it 'should be the correct length' do
       expect(get_SHA.length).to eq(40)
       expect(get_short_SHA.length).to eq(7)
