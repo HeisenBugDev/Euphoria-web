f3ae20d55f2224c33e3645a2ce5f73c035e8e3f8
Theron Boerner <hunterboerner@gmail.com>
2014-01-03 10:54:55 -0600

    Fix more flash stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,14 @@ module ApplicationHelper
     get_SHA.truncate(7, :omission => '')
   end
 
-  module ApplicationHelper
-    def flash_class(level)
-      case level
-        when :notice then
-          'info'
-        when :error then
-          'error'
-        when :alert then
-          'warning'
-      end
+  def flash_class(level)
+    case level
+      when :notice then
+        'info'
+      when :error then
+        'error'
+      when :alert then
+        'warning'
     end
   end
 end
