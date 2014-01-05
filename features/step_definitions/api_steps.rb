d29aa9fe867b3efc0fdb76f43f8ffbe2d2afba11
Theron Boerner <hunterboerner@gmail.com>
2014-01-05 11:50:51 -0600

    Add pending steps

---
+++ features/step_definitions/api_steps.rb
@@ -0,0 +1,71 @@
+Given(/^it tries to add a stat$/) do
+  pending
+end
+
+And(/^it does not specify a user$/) do
+  pending
+end
+
+Then(/^it should receive an error message$/) do
+  pending
+end
+
+And(/^the user does not exist$/) do
+  pending
+end
+
+Then(/^it should receive an unknown user message$/) do
+  pending
+end
+
+And(/^the user exists$/) do
+  pending
+end
+
+And(/^the stat does not exist$/) do
+  pending
+end
+
+Then(/^the stat should be added$/) do
+  pending
+end
+
+And(/^the stats count should be incremented$/) do
+  pending
+end
+
+And(/^it should receive a success message$/) do
+  pending
+end
+
+And(/^the stat exists$/) do
+  pending
+end
+
+Then(/^the stat's count should increment$/) do
+  pending
+end
+
+Given(/^It requests an unsecured page$/) do
+  pending
+end
+
+Given(/^It does not provide authentication$/) do
+  pending
+end
+
+Then(/^It should receive an unauthorized response$/) do
+  pending
+end
+
+Given(/^It provides authentication$/) do
+  pending
+end
+
+Then(/^It should receive a success message$/) do
+  pending
+end
+
+And(/^The page is secured$/) do
+  pending
+end
