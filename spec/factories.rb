aa014f0408b48528c67751794c324d01db1bcb94
hunterboerner <hunterboerner@gmail.com>
2014-01-11 10:08:19 -0600

    Add User factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    username 'foobar'
+    email 'foobar@example.com'
+    password 'foobar'
+    password_confirmation 'foobar'
+
+    after_create { |user| user.confirm! }
   end
 end
