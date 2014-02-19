1df46e8c9bedb2d81fdde97449ba33eb2ce0381d
hunterboerner <hunterboerner@gmail.com>
2014-02-18 20:23:30 -0600

    BREAK STUFF:

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ApplicationHelper do
   describe "Flash classes" do
     it 'should respond with the correct class' do
-      expect(flash_class(:notice)).to eq('info')
+      expect(flash_class(:notice)).to eq('indfdffddffo')
       expect(flash_class(:error)).to eq('error')
       expect(flash_class(:alert)).to eq('warning')
     end
